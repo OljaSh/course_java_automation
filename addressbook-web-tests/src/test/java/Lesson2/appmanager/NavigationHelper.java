79b5ad0650f9f94f0a2043b0077531342f67ad5e
sh.olja <sh.olja@gmail.com>
2020-11-06 22:51:36 +0200

    Lesson 2, commit 2.2 and 2.8 lessons

---
+++ addressbook-web-tests/src/test/java/Lesson2/appmanager/NavigationHelper.java
@@ -0,0 +1,15 @@
+package Lesson2.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigationHelper extends HelperBase{
+
+    public NavigationHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void gotoGroupPage() {
+      click(By.linkText("groups"));
+    }
+}
--- addressbook-web-tests/src/test/java/Lesson2/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/Lesson2/tests/GroupCreationTest.java
@@ -7,11 +7,16 @@ public class GroupCreationTest extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.gotoGroupPage();
-    app.initGroupCreation("new");
-    app.fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.submitGroupCreation("submit");
-    app.returnToGroupPage();
+    app.getNavigationHelper()
+            .gotoGroupPage();
+    app.getGroupHelper()
+            .initGroupCreation("new");
+    app.getGroupHelper()
+            .fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper()
+            .submitGroupCreation("submit");
+    app.getGroupHelper()
+            .returnToGroupPage();
     app.clickLogout("Logout");
   }
 
